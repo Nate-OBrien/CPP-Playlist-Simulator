1f810477d59e18f73e8b5958b9198b9d9a9f2532
Nate-OBrien <nathan.obrien@mcvts.org>
2025-01-29 10:38:26 -0500

    music works!

--- main.cpp
+++ main.cpp
@@ -1,1 +1,17 @@
+#include <windows.h>
 #include <iostream>
+#include <MMsystem.h>
+#pragma comment(lib, "winmm.lib" )
+
+int main() {
+    if (PlaySound(TEXT("FEIN.wav"), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP)) {
+        std::cout << "Sound is playing!" << std::endl;
+    } else {
+        std::cout << "Failed to play sound." << std::endl;
+    }
+    
+    // Add a pause to keep the program running while sound plays
+    std::cout << "Press any key to exit..." << std::endl;
+    std::cin.get();
+    return 0;
+}
